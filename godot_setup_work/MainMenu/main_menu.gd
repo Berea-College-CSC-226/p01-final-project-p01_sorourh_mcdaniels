e0ce60e656d1ce5b689eca3f180ebff086ec8684
sorourh05 <sorourh@berea.edu>
2026-05-06 02:04:51 -0400

    updated comments of the two screens

--- godot_setup_work/MainMenu/main_menu.gd
+++ godot_setup_work/MainMenu/main_menu.gd
@@ -1,8 +1,9 @@
 extends Control
 
-
+## When the start button is pressed, go to the world scene where the actual gameplay is
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://World/world.tscn")
 
+## When the exit button is pressed, quit the program
 func _on_exit_pressed() -> void:
 	get_tree().quit()
